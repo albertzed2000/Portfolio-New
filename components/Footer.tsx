a8be7369f4907d036de741c3a5b8465fbdc58fd8
Albert Zheng <albert.zheng@mail.utoronto.ca>
2022-05-31 23:25:29 -0400

    add contact icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,8 @@
 import styles from '../styles/Home.module.css'
 
+import {AiFillGithub, AiFillLinkedin, AiFillMail} from 'react-icons/ai'
+import {FaToiletPaper} from 'react-icons/fa'
+
 const Footer = (): JSX.Element => (
     <footer className={styles.footer}>
         <span className={styles.footerCopyright}>
@@ -7,7 +10,24 @@ const Footer = (): JSX.Element => (
         </span>
           
         <span className={styles.footerIconsContainer}>
-            ICON HYPERLINKS GO HERE
+
+            <a id={styles.footerGithubIcon} href='https://github.com/albertzed2000' target='blank'>
+            <AiFillGithub/>
+            </a>
+
+            <a id={styles.footerLinkedinIcon} href='https://www.linkedin.com/in/albertzed/' target='blank'>
+            <AiFillLinkedin/>
+            </a>
+
+            <a id={styles.footerMailIcon} href='mailto:mralbertzheng@gmail.com' target='blank'>
+            <AiFillMail/>
+            </a>
+
+            <a id={styles.footerResumeIcon} href='https://drive.google.com/file/d/1UiSaKY1HgD1ayzoK8QDWVDTAIjo0lBpu/view?usp=sharing' target='blank'>
+            <FaToiletPaper/>
+            </a>
+
+
         </span>
     </footer>
 )
