b8e70ddecc3a70121ad7fbf02b0a6b2acc1711d6
Albert Zheng <albert.zheng@mail.utoronto.ca>
2023-04-09 16:40:49 -0400

    add alt text for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,11 +4,19 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Back from '../components/Back'
-
+import { useState } from 'react'
 import {AiFillGithub, AiFillLinkedin, AiFillMail} from 'react-icons/ai'
 import {FaToiletPaper} from 'react-icons/fa'
 
 const Contact: NextPage = () => {
+
+  let [currText, setCurrText] = useState("");
+
+  const onHover = (newText: string) => {
+
+    setCurrText(newText)
+  }
+
   return (
     <div className={styles.pageContainer}>
       <span className={styles.title}>
@@ -19,24 +27,38 @@ const Contact: NextPage = () => {
 
         <div className={styles.contactIcons}>
 
-          <a href='https://github.com/albertzed2000' target='blank'>
+          <a onMouseEnter={()=>{setCurrText("GITHUB")}}
+            onMouseLeave={()=>{setCurrText("")}}
+           href='https://github.com/albertzed2000' target='blank'>
           <AiFillGithub/>
           </a>
 
-          <a href='https://www.linkedin.com/in/albertzed/' target='blank'>
+          <a onMouseEnter={()=>{setCurrText("LINKEDIN")}}
+            onMouseLeave={()=>{setCurrText("")}}
+          href='https://www.linkedin.com/in/albertzed/' target='blank'>
           <AiFillLinkedin/>
           </a>
 
-          <a href='mailto:mralbertzheng@gmail.com' target='blank'>
+          <a onMouseEnter={()=>{setCurrText("EMAIL")}}
+            onMouseLeave={()=>{setCurrText("")}}
+          href='mailto:mralbertzheng@gmail.com' target='blank'>
           <AiFillMail/>
           </a>
 
-          <a href='https://drive.google.com/file/d/1UiSaKY1HgD1ayzoK8QDWVDTAIjo0lBpu/view?usp=sharing' target='blank'>
+          <a onMouseEnter={()=>{setCurrText("RESUME")}}
+            onMouseLeave={()=>{setCurrText("")}}
+          href='https://drive.google.com/file/d/1UiSaKY1HgD1ayzoK8QDWVDTAIjo0lBpu/view?usp=sharing' target='blank'>
           <FaToiletPaper/>
           </a>
 
 
+
+
         </div>
+        
+        <div className={styles.contactAltText}>
+            {currText}
+          </div>
 
         <Back color="#9BC53D"/>
     </div>
